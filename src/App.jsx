cea47fb7d210151f3babab0f1e8056a8fa6ae6b1
Juanes-xd <juanestebanortizbejarano@gmail.com>
2021-12-06 00:44:10 -0500

    Entrar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ListadoNames from "./components/ListadoNames";
+
+function App(){
+  return (
+    <div className="container">
+        <ListadoNames/>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/ListadoNames.jsx
@@ -0,0 +1,12 @@
+import React,{useState} from "react"
+
+
+const ListadoNames = () =>{
+    return (
+        <div>
+            <h2>Aplicacion de crud basica</h2>
+        </div>
+    )
+}
+
+export default ListadoNames
