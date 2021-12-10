1bf6d7d6d088c86736079667da273c83a4308079
Juanes-xd <juanestebanortizbejarano@gmail.com>
2021-12-10 01:26:59 -0500

    Nuevo cambio

--- src/components/ListadoNames.jsx
+++ src/components/ListadoNames.jsx
@@ -5,6 +5,9 @@ const ListadoNames = () =>{
     return (
         <div>
             <h2>Aplicacion de crud basic</h2>
+            <div className="row"></div>
+            <div className="col">Listado Nombres</div>
+            <div className="col">Formulario para añadir nombres</div>
         </div>
     )
 }
