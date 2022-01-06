c095621abd35f2d233c46b33f8cd259f58b2a3e8
Juanes-xd <juanestebanortizbejarano@gmail.com>
2022-01-05 19:56:09 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Bienvenida from "./components/Bienvenida"
 function App(){
   return (
     <div className="container">
-       {/* <ListadoNames/> */}
-       <h1>Propiedades de componentes</h1>
+        <ListadoNames/> 
+      { /*<h1>Propiedades de componentes</h1>
        <Bienvenida nombre="Willy"/>
        <Bienvenida nombre="Camilo"/>
        <Bienvenida nombre="Juanes"/>
-       <Bienvenida nombre="Caro"/>
+      <Bienvenida nombre="Caro"/>*/}
     </div>
   );
 }
